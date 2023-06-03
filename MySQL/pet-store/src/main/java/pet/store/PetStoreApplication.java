577c234f3e901a24378d3fbc24dadce4b3b79a45
mzenni <116816541+mzenni@users.noreply.github.com>
2023-06-03 19:21:05 -0400

    Firrst commit spring boot with database not updating:

---
+++ MySQL/pet-store/src/main/java/pet/store/PetStoreApplication.java
@@ -0,0 +1,12 @@
+package pet.store;
+
+import org.springframework.boot.SpringApplication;
+
+public class PetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApplication.class, args);
+
+	}
+
+}
