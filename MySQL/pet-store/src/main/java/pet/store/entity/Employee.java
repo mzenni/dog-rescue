f07112bd10b13ba4237663fa4d9d3a574b4727aa
mzenni <116816541+mzenni@users.noreply.github.com>
2023-06-03 21:54:44 -0400

    project fixed

--- MySQL/pet-store/src/main/java/pet/store/entity/Employee.java
+++ MySQL/pet-store/src/main/java/pet/store/entity/Employee.java
@@ -18,7 +18,6 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long employeeId;
-	//private Long petStoreId;
 	private String employeeFirstName;
 	private String employeeLastName;
 	private String employeePhone;
